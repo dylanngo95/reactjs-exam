2c683a58f77122a775c06d9eee27fc48aecfdf94
Dylan Ngo <it.tinhngo@gmail.com>
2022-01-16 10:02:14 +0700

    remove threejs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Parent } from './components/portals/Portal';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root'),
+  document.getElementById('app-root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
