46251602fd5e297daf77cd5b084b137e8874c4de
Dylan Ngo <it.tinhngo@gmail.com>
2022-01-20 21:49:31 +0700

    support tailwindcss

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
             <Header />
             <div className='Home'>
                 <p>Home Component {homeContext.count}</p>
-                <button onClick={() => { homeContext.dispatch('increment') }} >Increase</button>
-                <button onClick={() => { homeContext.dispatch('decrement') }} >Decrease</button>
-                <button onClick={() => { homeContext.dispatch('reset') }} >Reset</button>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={() => { homeContext.dispatch('increment') }} >Increase</button>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={() => { homeContext.dispatch('decrement') }} >Decrease</button>
+                <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={() => { homeContext.dispatch('reset') }} >Reset</button>
             </div>
             <Footer />
         </>
