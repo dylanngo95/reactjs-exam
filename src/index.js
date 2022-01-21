6a512e5ff55cafadbe705eebe913ae897c3146a7
Dylan Ngo <tinh.ngo@ggg.com.vn>
2022-01-21 12:52:59 +0700

    update customer image

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <App />,
