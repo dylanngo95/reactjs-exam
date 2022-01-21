3a1886333f4f7b2aa0e253a7df6472f6d969ba3e
Dylan Ngo <tinh.ngo@ggg.com.vn>
2022-01-21 13:39:29 +0700

    update item

--- src/Context/HomeContext.js
+++ src/Context/HomeContext.js
@@ -15,28 +15,28 @@ export const initialHomeState = [
     {
         id: 1,
         post: `Thanks to @tailwindcss, CSS started to make sense to me.`,
-        image: 'post.jpg',
+        image: 'post6.jpg',
         name: 'Nuno Maduro',
         companyName: 'Core Team Member @laravelphp'
     },
     {
         id: 2,
         post: `If I had to recommend a way of getting into programming today, it would be HTML + CSS with Tailwind CSS.`,
-        image: 'post.jpg',
+        image: 'post2.jpg',
         name: 'Guillermo Rauch',
         companyName: 'Vercel'
     },
     {
         id: 3,
         post: `With Tailwind I can offer my clients faster turnaround times on custom WordPress themes, both for initial builds and for future revisions.`,
-        image: 'post.jpg',
+        image: 'post3.jpg',
         name: 'Greg Sullivan',
         companyName: 'WordPress Developer'
     },
     {
         id: 4,
         post: `Tailwind has been a total game-changer for our dev team. It allows us to move faster, keep our UI consistent, and focus on the work we want to do instead of writing CSS.`,
-        image: 'post6.jpg',
+        image: 'post4.jpg',
         name: 'Jake Ryan Smith',
         companyName: 'WordPress Developer'
     }
