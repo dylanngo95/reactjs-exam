b92f88599627b5f62e24d89b245214db7f3391e0
Dylan Ngo <it.tinhngo@gmail.com>
2022-01-24 12:15:29 +0700

    fix button on ios

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -98,8 +98,8 @@ const Cart = () => {
                         <p className="text-sm text-coolGray-600">Not including taxes and shipping costs</p>
                     </div>
                     <div className="flex justify-end space-x-4">
-                        <a href="/" type="button" className="px-6 py-2 border rounded-md border-black-600 text-coolGray-50">Back
-                            <span className="sr-only sm:not-sr-only"> to shop</span>
+                        <a href="/" type="button" className="px-6 py-2 border rounded-md border-black-600 text-coolGray-50">Back to
+                            <span className="sr-only sm:not-sr-only"> shop</span>
                         </a>
                         <button type="button" className="px-6 py-2 border rounded-md text-white bg-red-500 text-coolGray-50">
                             <span className="sr-only sm:not-sr-only">Continue to</span> Checkout
